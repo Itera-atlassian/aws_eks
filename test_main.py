01c500a8ed917772f0353bea17e9690774592f84
fabio fabio gualtero <fegualterog@gmail.com>
2023-10-06 03:26:07 +0000

    Added test_main.py

---
+++ test_main.py
@@ -0,0 +1,35 @@
+import pytest
+from fastapi.testclient import TestClient
+from main import app
+
+# Create a TestClient instance for your FastAPI app
+client = TestClient(app)
+
+def test_home_endpoint():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert "kubectl" in response.text
+
+def test_healthz_endpoint():
+    response = client.get("/healthz")
+    assert response.status_code == 200
+    assert response.json() == {"status": "OK"}
+
+def test_sumatoria_endpoint():
+    # Verifica que "sumatoria" sea igual a 16
+    response = client.get("/sumatoria/5", headers={"materia": "calculo"})
+    assert response.status_code == 200
+    assert response.json().get("sumatoria") == 16  
+
+    # Test an invalid request (negative number)
+    response = client.get("/sumatoria/-5", headers={"materia": "calculo"})
+    assert response.status_code == 200
+    assert response.json() == {"error": "El número debe ser no negativo"}
+
+    # Test an invalid request (wrong 'materia' header)
+    response = client.get("/sumatoria/5", headers={"materia": "invalid"})
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Header 'materia' must be 'calculo'"}
+
+if __name__ == "__main__":
+    pytest.main()
